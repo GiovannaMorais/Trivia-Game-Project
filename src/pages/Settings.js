77e5f9f36b27e5e05ffc5cdf9843d0cf327df4e9
Giovane <giovanedaniell@gmail.com>
2022-06-29 12:24:09 -0300

    requisitos 9, 11, 12, 13

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export default class Settings extends Component {
   render() {
     return (
-      <h1 data-testid="settings-title">Settings</h1>
+      <h1 data-testid="settings-title">Set1tings</h1>
     );
   }
 }
